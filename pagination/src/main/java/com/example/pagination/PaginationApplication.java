528bc0a9474de80eb66344faabdf31a8f8e5ccb7
suresh121982 <sureshvaikundam@gmail.com>
2026-03-01 10:54:13 +0530

    modified files added logging on 1/2/2026

--- pagination/src/main/java/com/example/pagination/PaginationApplication.java
+++ pagination/src/main/java/com/example/pagination/PaginationApplication.java
@@ -1,12 +1,23 @@
 package com.example.pagination;
 
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
+@Slf4j
 public class PaginationApplication {
 
 	public static void main(String[] args) {
+		   final Logger logger = LoggerFactory.getLogger(PaginationApplication.class);
+		   logger.info("Main Class is Started");
+
 		SpringApplication.run(PaginationApplication.class, args);
 	}
 
--- pagination/src/main/java/com/example/pagination/service/UserService.java
+++ pagination/src/main/java/com/example/pagination/service/UserService.java
@@ -1,7 +1,10 @@
 package com.example.pagination.service;
 
+import com.example.pagination.PaginationApplication;
 import com.example.pagination.model.User;
 import com.example.pagination.repository.UserReposirtory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -12,10 +15,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
+    final Logger logger = LoggerFactory.getLogger(UserService.class);
+
     @Autowired
     private UserReposirtory userRepository;
 
     public Page<User> getUsers(int page,int size){
+
+        logger.info("User Service is called");
 
         Pageable pageable = PageRequest.of(page, size, Sort.by("username").ascending());
 
